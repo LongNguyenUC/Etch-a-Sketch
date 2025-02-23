e507c7bc154ac7e4f0b89e06e0022841223b4d0a
LongNguyenUC <192123324+LongNguyenUC@users.noreply.github.com>
2025-02-22 23:04:56 -0500

    Add button to change canvas size

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 const canvas = document.querySelector(".canvas-container");
+const changeSizeButton = document.querySelector("#changeSizeButton")
 const width = 480;
 const height = 480;
 
-displaySquares();
+displaySquares(16);
 
 
-function displaySquares(){
+function displaySquares(squareAmount){
     console.log("starting!");
     console.log(width);
-    for(let i = 0; i < 16; i++){
-        for(let j = 0; j < 16; j++){
+    for(let i = 0; i < squareAmount; i++){
+        for(let j = 0; j < squareAmount; j++){
             let square = document.createElement("div");
             square.classList.add("square")
-            square.style.width = width/16 + "px";
-            square.style.height = height/16 + "px";
+            square.style.width = width/squareAmount + "px";
+            square.style.height = height/squareAmount + "px";
             square.addEventListener("mouseover", colorSquare);
             canvas.appendChild(square);
         }
@@ -23,3 +24,13 @@ function displaySquares(){
 function colorSquare(e){
     e.target.style.backgroundColor = "black";
 }
+
+function changeCanvasSize(){
+    let squareAmount = prompt("Enter a value to change the number of squares per side for the canvas.", "100");
+    if(!isNaN(squareAmount) && squareAmount <= 100 && squareAmount >= 1){
+        canvas.textContent ="";
+        displaySquares(squareAmount);
+    }
+}
+
+changeSizeButton.addEventListener("click", changeCanvasSize);
